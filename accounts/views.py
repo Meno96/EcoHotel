d36f836905934599df0cf3c3c2c5579df85efdf9
Daniele <danielemeno96@gmail.com>
2022-12-05 16:58:26 +0100

    Finale

--- accounts/views.py
+++ accounts/views.py
@@ -5,7 +5,11 @@ from django.views.decorators.csrf import csrf_exempt
 from django.contrib import messages
 from django.contrib.auth import authenticate, login, logout
 from django.contrib.auth.decorators import login_required
+from django.http import JsonResponse, HttpResponse
 from datetime import datetime
+from .models import Consumi
+import json
+
 # from tkinter import *
 import tkinter as tk
 import tkinter.messagebox
@@ -24,6 +28,7 @@ def messageBox(message):
     button.pack(side="bottom", fill="none", expand=True)
     root.mainloop()
 
+
 @csrf_exempt
 def getActualIP(request):
     x_forwarded_for = request.META.get('HTTP_X_FORWARDED_FOR')
@@ -34,6 +39,7 @@ def getActualIP(request):
         ip = request.META.get('REMOTE_ADDR')
 
     return ip
+
 
 @csrf_exempt
 def addIp(actualIp):
@@ -50,28 +56,13 @@ def loginPage(request):
         password = request.POST.get('password')
         user = authenticate(request, username=username, password=password)
 
-        # Memorizza l’ultimo IP che ha avuto accesso alla 
-        # piattaforma per un admin, mostra un messaggio di 
-        # avvertimento quando questo è diverso dal precedente
-        if user.groups.filter(name='admin').exists():
-            dbIp = IpAddress.objects.all().values().last()
-            actualIp = getActualIP(request)
-            
-            if not dbIp:
-                addIp(actualIp)
-            else:
-                if actualIp!=dbIp['ip_address']:
-                    addIp(actualIp)
-                    messageBox("L'indirizzo IP corrente è diverso dal precedente con cui è stato effettuato il login!")
-
         if user is not None:
             login(request, user)
             return redirect('home')
         else:
             messages.info(request, 'Username o Password sono sbagliati')
 
-    context = {}
-    return render(request, 'accounts/login.html', context)
+    return render(request, 'accounts/login.html')
 
 
 @unauthenticated_user
@@ -104,10 +95,28 @@ def logoutUser(request):
 
 @csrf_exempt
 @login_required(login_url='login')
-@admin_only
 def consumiPage(request):
-    context = {}
-    return render(request, 'accounts/consumi.html', context)
+    if request.method == "POST":
+        body_unicode = request.body.decode('utf-8')
+        body = json.loads(body_unicode)
+        produced_energy_in_watt = body['produced_energy_in_watt']
+        consumed_energy_in_watt = body['consumed_energy_in_watt']
+
+        # print(body)
+        t = Consumi(produced_energy_in_watt=produced_energy_in_watt,
+                    consumed_energy_in_watt=consumed_energy_in_watt)
+        Consumi.writeOnChain(t)
+
+        # return JsonResponse(t, safe=False)
+
+    data = Consumi.objects.all()
+    totaleP = 0
+    totaleC = 0
+    for object in data:
+        totaleP += int(object.produced_energy_in_watt)
+        totaleC += int(object.consumed_energy_in_watt)
+
+    return render(request, 'accounts/consumi.html',  {'data': data, 'totaleP': totaleP, 'totaleC': totaleC})
 
 
 @csrf_exempt
@@ -115,9 +124,28 @@ def userPage(request):
     context = {}
     return render(request, 'accounts/user.html', context)
 
+
 @login_required(login_url='login')
 @csrf_exempt
 def homePage(request):
-    context = {}
 
-    return render(request, 'accounts/home.html', context)
+    # Memorizza l’ultimo IP che ha avuto accesso alla
+    # piattaforma per un admin, mostra un messaggio di
+    # avvertimento quando questo è diverso dal precedente
+    checkIp={'checkIp': None}
+    if request.user.is_staff:
+        dbIp = IpAddress.objects.all().values().last()
+        actualIp = getActualIP(request)
+
+        if not dbIp:
+            addIp(actualIp)
+        else:
+            if actualIp != dbIp['ip_address']:
+                addIp(actualIp)
+                checkIp={'checkIp': True}
+                # messageBox(
+                #     "L'indirizzo IP corrente è diverso dal precedente con cui è stato effettuato il login!")
+    
+    print(checkIp)
+
+    return render(request, 'accounts/home.html', checkIp)
