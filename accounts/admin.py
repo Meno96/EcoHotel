7d4c5dfc6cc1678af7145e80dd50dbf7773871c3
Daniele <danielemeno96@gmail.com>
2022-12-05 17:08:04 +0100

    Finale

--- accounts/admin.py
+++ accounts/admin.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
-from .models import IpAddress
+from .models import IpAddress, Consumi
 
 # Register your models here.
 
 admin.site.register(IpAddress)
+admin.site.register(Consumi)
